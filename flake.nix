ed88bdb1641cba126942ce5072890163009126bd
rop borney <birnxy@gmail.com>
2025-01-20 17:13:26 -0800

    ljl

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
