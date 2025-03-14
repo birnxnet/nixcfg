6ff0d78a50aed5e705f923e3cdebbc65ad1e8760
rop borney <birnxy@gmail.com>
2025-03-14 12:24:55 -0700

    ljljlj

--- pkgs/monolisa/default.nix
+++ pkgs/monolisa/default.nix
@@ -1,23 +1,22 @@
-{ lib, fetchzip }:
+{ lib, fetchurl }:
 
 let
+  pname = "monolisa";
   version = "2.015";
 in
-fetchzip {
-  name = "monolisa-${version}";
-
-  url = " https://fourdaycreep.com/s/rBnnEYSWCAeeE6f/download/monolisa.zip";
+fetchFromGitHub {
+  name = "${pname}-${version}";
+  url = "https://fourdaycreep.com/s/E3yZLKPpLow9w4H/download/monolisa.tar.gz";
+  sha256 = "sha256-jUAF7hSr2DSF3qcsmmu2LrkHntEo6gf3KEvNzlyQu7M=";
 
   postFetch = ''
-    unzip -j $downloadedFile --strip=1
+    tar xf $downloadedFile --strip=1
     install -m444 -Dt $out/share/fonts/truetype fonts/ttf/*.ttf
   '';
 
-  sha256 = "sha256-d9oXhgaRwlxuPvLzp0o74ChKs9jWUhkn8mY1nNmetOU=";
-
   meta = with lib; {
+    longdescription = "MonoLisa was designed by professionals to improve developers’ productivity and reduce fatigue.";
     homepage = "https://monolisa.dev";
-    description = "MonoLisa was designed by professionals to improve developers’ productivity and reduce fatigue.";
     platforms = platforms.all;
   };
 }
