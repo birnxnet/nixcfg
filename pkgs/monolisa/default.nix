5cdeb362e3f8c5800085ce5f46d28927cad40755
rop borney <birnxy@gmail.com>
2025-03-14 12:44:37 -0700

    lkj

--- pkgs/monolisa/default.nix
+++ pkgs/monolisa/default.nix
@@ -1,17 +1,17 @@
 { lib, fetchurl }:
 
 let
-  pname = "monolisa";
+  name = "monolisa";
   version = "2.015";
 in
 fetchurl {
-  name = "${pname}-${version}";
+  name = "monolisa";
   url = "https://fourdaycreep.com/s/E3yZLKPpLow9w4H/download/monolisa.tar.gz";
   sha256 = "sha256-jUAF7hSr2DSF3qcsmmu2LrkHntEo6gf3KEvNzlyQu7M=";
 
   postFetch = ''
     tar xf $downloadedFile --strip=1
-    install -m444 -Dt $out/share/fonts/truetype fonts/ttf/*.ttf
+    install -m665 -Dt $out/share/fonts/truetype fonts/ttf/*.ttf
   '';
 
   meta = with lib; {
