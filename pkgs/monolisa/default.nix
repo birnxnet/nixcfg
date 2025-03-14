3127b809f4c2d79afdf00d7f765148e762e0845a
rop borney <birnxy@gmail.com>
2025-03-14 12:27:13 -0700

    ljl

--- pkgs/monolisa/default.nix
+++ pkgs/monolisa/default.nix
@@ -4,7 +4,7 @@ let
   pname = "monolisa";
   version = "2.015";
 in
-fetchFromGitHub {
+fetchurl {
   name = "${pname}-${version}";
   url = "https://fourdaycreep.com/s/E3yZLKPpLow9w4H/download/monolisa.tar.gz";
   sha256 = "sha256-jUAF7hSr2DSF3qcsmmu2LrkHntEo6gf3KEvNzlyQu7M=";
