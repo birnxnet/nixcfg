22a84369c03a44cc81ec071a5cabb7e6ac587aa9
rop borney <birnxy@gmail.com>
2025-03-23 00:13:48 -0700

    lkjlk

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     # };
 
     # nix-fonts.url = "github:birnxnet/nix-fonts";
-    nvf.url = "https://flakehub.com/f/birnxnet/nvf/0.1.1925";
+    nvf.url = "https://flakehub.com/f/birnxnet/nvf/0.1.1925.tar.gz";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
   };
 
